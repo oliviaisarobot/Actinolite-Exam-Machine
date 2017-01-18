2f3eb3ff8796ee742c137ccc1775d5b5a560d243
Máté Szurovecz <szuri92@gmail.com>
2017-01-18 16:34:14 +0100

    rewrite server

--- fakeserver/fakeserver.js
+++ fakeserver/fakeserver.js
@@ -1,19 +1,31 @@
 'use strict';
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var path = require('path');
 var express = require('express');
 var mysql = require('mysql');
 
+var index = require('./routes/index')
 var app = express();
+
+
+app.set('view', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, '../exam-machine/src')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/', index);
 
-//app.use(express.static('./../exam-machine/src'));
-
-
-///
-// app.post('/user', function(req, res) {
-//
+// app.post('/signup', function(res, req) {
+//   con.query('')
 // });
+
+
+
 //var port = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(5000, function() {
+  console.log('server started on port 5000');
+});
