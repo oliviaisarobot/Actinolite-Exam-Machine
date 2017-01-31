6cedb21368552bdc42edf26947d8f720bd681fae
Olivia Gardiner <oliv.gardiner@gmail.com>
2017-01-31 14:56:49 +0100

    header fix???

--- server.js
+++ server.js
@@ -20,4 +20,10 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(process.env.PORT || 8080);
