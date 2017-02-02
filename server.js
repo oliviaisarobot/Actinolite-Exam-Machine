83bf0f1d911c517ffba65a37bb9ad5a60e93e6f1
Olivia Gardiner <oliv.gardiner@gmail.com>
2017-02-02 13:20:16 +0100

    something about the server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist'));
 });
 
 app.use(function(req, res, next) {
